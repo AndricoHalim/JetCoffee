016d4b9bdf72ff87ff40d8a59b37822aeb747cb7
AndricoHalim <andrico.21421007@student.ubl.ac.id>
2023-10-29 22:05:28 +0700

    Menambahkan Category Item

--- app/src/main/java/com/andricohalim/jetcoffee/MainActivity.kt
+++ app/src/main/java/com/andricohalim/jetcoffee/MainActivity.kt
@@ -3,13 +3,21 @@ package com.andricohalim.jetcoffee
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.andricohalim.jetcoffee.ui.components.Search
 import com.andricohalim.jetcoffee.ui.theme.JetCoffeeTheme
 
 class MainActivity : ComponentActivity() {
@@ -25,6 +33,24 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun JetCoffeeApp() {
+    Column {
+        Banner()
+    }
+}
+
+@Composable
+fun Banner(
+    modifier: Modifier = Modifier,
+) {
+    Box(modifier = modifier) {
+        Image(
+            painter = painterResource(R.drawable.banner),
+            contentDescription = "Banner Image",
+            contentScale = ContentScale.Crop,
+            modifier = Modifier.height(160.dp)
+        )
+        Search()
+    }
 }
 
 @Preview(showBackground = true)
