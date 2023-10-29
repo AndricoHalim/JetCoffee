28c7d02be1f02b925bbfe4a67bfca31edf1e2fe0
AndricoHalim <andrico.21421007@student.ubl.ac.id>
2023-10-29 23:11:04 +0700

    Menambahkan Bottom Bar

--- app/src/main/java/com/andricohalim/jetcoffee/MainActivity.kt
+++ app/src/main/java/com/andricohalim/jetcoffee/MainActivity.kt
@@ -8,15 +8,21 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AccountCircle
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
@@ -25,6 +31,7 @@ import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.andricohalim.jetcoffee.model.BottomBarItem
 import com.andricohalim.jetcoffee.model.Menu
 import com.andricohalim.jetcoffee.model.dummyBestSellerMenu
 import com.andricohalim.jetcoffee.model.dummyCategory
@@ -33,7 +40,6 @@ import com.andricohalim.jetcoffee.ui.components.CategoryItem
 import com.andricohalim.jetcoffee.ui.components.HomeSection
 import com.andricohalim.jetcoffee.ui.components.MenuItem
 import com.andricohalim.jetcoffee.ui.components.Search
-import com.andricohalim.jetcoffee.ui.components.SectionText
 import com.andricohalim.jetcoffee.ui.theme.JetCoffeeTheme
 
 class MainActivity : ComponentActivity() {
@@ -48,20 +54,28 @@ class MainActivity : ComponentActivity() {
 }
 @Composable
 fun JetCoffeeApp() {
-    Column(modifier = Modifier.verticalScroll(rememberScrollState())) {
-        Banner()
-        HomeSection(
-            title = stringResource(R.string.section_category),
-            content = { CategoryRow() }
-        )
-        HomeSection(
-            title = stringResource(R.string.menu_favorite),
-            content = { MenuRow(dummyMenu)}
-        )
-        HomeSection(
-            title = stringResource(R.string.section_best_seller_menu),
-            content = { MenuRow(dummyBestSellerMenu)}
-        )
+    Scaffold(
+        bottomBar = { BottomBar() }
+    ) { innerPadding ->
+        Column(
+            modifier = Modifier
+                .verticalScroll(rememberScrollState())
+                .padding(innerPadding)
+        ) {
+            Banner()
+            HomeSection(
+                title = stringResource(R.string.section_category),
+                content = { CategoryRow() }
+            )
+            HomeSection(
+                title = stringResource(R.string.menu_favorite),
+                content = { MenuRow(dummyMenu) }
+            )
+            HomeSection(
+                title = stringResource(R.string.section_best_seller_menu),
+                content = { MenuRow(dummyBestSellerMenu) }
+            )
+        }
     }
 }
 
@@ -107,6 +121,46 @@ fun MenuRow(
     ) {
         items(listMenu, key = { it.title }) { menu ->
             MenuItem(menu)
+        }
+    }
+}
+
+@Composable
+fun BottomBar(
+    modifier: Modifier = Modifier
+) {
+    NavigationBar(
+        containerColor = MaterialTheme.colorScheme.background,
+        modifier = modifier
+    ) {
+        val navigationItems = listOf(
+            BottomBarItem(
+                title = stringResource(R.string.menu_home),
+                icon = Icons.Default.Home
+            ),
+            BottomBarItem(
+                title = stringResource(R.string.menu_favorite),
+                icon = Icons.Default.Favorite
+            ),
+            BottomBarItem(
+                title = stringResource(R.string.menu_profile),
+                icon = Icons.Default.AccountCircle
+            ),
+        )
+        navigationItems.map {
+            NavigationBarItem(
+                icon = {
+                    Icon(
+                        imageVector = it.icon,
+                        contentDescription = it.title
+                    )
+                },
+                label = {
+                    Text(it.title)
+                },
+                selected = it.title == navigationItems[0].title,
+                onClick = {}
+            )
         }
     }
 }
